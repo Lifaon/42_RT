365b849c1a9d60869e21d9a61df6459485e71469
Meriadec LANTONNET <mlantonn@student.42.fr>
2018-05-23 16:23:29 +0200

    corrected cone radius bug

--- srcs/vec/intersect.c
+++ srcs/vec/intersect.c
@@ -6,11 +6,30 @@
 /*   By: mlantonn <mlantonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/11 16:34:49 by mlantonn          #+#    #+#             */
-/*   Updated: 2018/05/22 14:37:52 by mlantonn         ###   ########.fr       */
+/*   Updated: 2018/05/23 16:21:59 by mlantonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
+
+int		solve_quadratic_equation(t_inter *inter, double a, double b, double c)
+{
+	inter->delta = (b * b) - 4 * a * c;
+	if (inter->delta < 0)
+		return (0);
+	inter->t1 = (-b + sqrt(inter->delta)) / (2 * a);
+	inter->t2 = (-b - sqrt(inter->delta)) / (2 * a);
+	if (inter->t1 > inter->min_dist && inter->t2 > inter->min_dist)
+		inter->t = inter->t1 < inter->t2 ? inter->t1 : inter->t2;
+	else
+	{
+		if (inter->t1 > inter->min_dist || inter->t2 > inter->min_dist)
+			inter->t = inter->t1 > inter->t2 ? inter->t1 : inter->t2;
+		else
+			return (0);
+	}
+	return (1);
+}
 
 int		intersect_sphere(t_obj sphere, t_vec ray, t_inter *inter)
 {
@@ -24,16 +43,7 @@ int		intersect_sphere(t_obj sphere, t_vec ray, t_inter *inter)
 		return (0);
 	inter->t1 = (-b + sqrt(inter->delta)) / 2;
 	inter->t2 = (-b - sqrt(inter->delta)) / 2;
-	if (inter->t1 > inter->min_dist && inter->t2 > inter->min_dist)
-		inter->t = inter->t1 < inter->t2 ? inter->t1 : inter->t2;
-	else
-	{
-		if (inter->t1 > inter->min_dist || inter->t2 > inter->min_dist)
-			inter->t = inter->t1 > inter->t2 ? inter->t1 : inter->t2;
-		else
-			return (0);
-	}
-	return (1);
+	return (solve_quadratic_equation(inter, 1, b, c));
 }
 
 int		intersect_plane(t_obj plane, t_vec ray, t_inter *inter)
@@ -63,21 +73,7 @@ int		intersect_cylinder(t_obj cyl, t_vec ray, t_inter *inter)
 		dot_product(ray, cyl.dir) * dot_product(inter->oc, cyl.dir));
 	c = dot_product(inter->oc, inter->oc) - ((dot_product(inter->oc, cyl.dir) *\
 		dot_product(inter->oc, cyl.dir)) + (cyl.r * cyl.r));
-	inter->delta = (b * b) - 4 * a * c;
-	if (inter->delta < 0)
-		return (0);
-	inter->t1 = (-b + sqrt(inter->delta)) / (2 * a);
-	inter->t2 = (-b - sqrt(inter->delta)) / (2 * a);
-	if (inter->t1 > inter->min_dist && inter->t2 > inter->min_dist)
-		inter->t = inter->t1 < inter->t2 ? inter->t1 : inter->t2;
-	else
-	{
-		if (inter->t1 > inter->min_dist || inter->t2 > inter->min_dist)
-			inter->t = inter->t1 > inter->t2 ? inter->t1 : inter->t2;
-		else
-			return (0);
-	}
-	return (1);
+	return (solve_quadratic_equation(inter, a, b, c));
 }
 
 int		intersect_cone(t_obj cone, t_vec ray, t_inter *inter)
@@ -85,26 +81,14 @@ int		intersect_cone(t_obj cone, t_vec ray, t_inter *inter)
 	double	a;
 	double	b;
 	double	c;
+	double	radius;
 
-	a = dot_product(ray, ray) - (1 + (tan(cone.r) * tan(cone.r))) * \
+	radius = 1 + (cone.r * M_PI / 180) * (cone.r * M_PI / 180);
+	a = 1 - radius * \
 		(dot_product(ray, cone.dir) * dot_product(ray, cone.dir));
-	b = 2 * (dot_product(ray, inter->oc) - (1 + (tan(cone.r) * tan(cone.r))) * \
+	b = 2 * (dot_product(ray, inter->oc) - radius * \
 		dot_product(ray, cone.dir) * dot_product(inter->oc, cone.dir));
-	c = dot_product(inter->oc, inter->oc) - (1 + (tan(cone.r) * tan(cone.r))) *\
+	c = dot_product(inter->oc, inter->oc) - radius * \
 		(dot_product(inter->oc, cone.dir) * dot_product(inter->oc, cone.dir));
-	inter->delta = (b * b) - 4 * a * c;
-	if (inter->delta < 0)
-		return (0);
-	inter->t1 = (-b + sqrt(inter->delta)) / (2 * a);
-	inter->t2 = (-b - sqrt(inter->delta)) / (2 * a);
-	if (inter->t1 > inter->min_dist && inter->t2 > inter->min_dist)
-		inter->t = inter->t1 < inter->t2 ? inter->t1 : inter->t2;
-	else
-	{
-		if (inter->t1 > inter->min_dist || inter->t2 > inter->min_dist)
-			inter->t = inter->t1 > inter->t2 ? inter->t1 : inter->t2;
-		else
-			return (0);
-	}
-	return (1);
+	return (solve_quadratic_equation(inter, a, b, c));
 }

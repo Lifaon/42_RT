50a1d91803ea5ca473b64020bb094cb9a495622c
Mériadec LANTONNET <mlantonn@student.42.fr>
2018-05-28 19:07:36 +0200

    Phong completed

--- srcs/draw/shade.c
+++ srcs/draw/shade.c
@@ -6,13 +6,13 @@
 /*   By: mlantonn <mlantonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 05:22:06 by mlantonn          #+#    #+#             */
-/*   Updated: 2018/05/25 16:04:01 by mlantonn         ###   ########.fr       */
+/*   Updated: 2018/05/28 18:59:13 by mlantonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-int		is_light_path_blocked(t_data *data, t_vec ip, t_vec light, double len)
+int		light_is_path_blocked(t_data *data, t_vec ip, t_vec light, double len)
 {
 	t_inter	it;
 	int		i;
@@ -30,9 +30,7 @@ int		is_light_path_blocked(t_data *data, t_vec ip, t_vec light, double len)
 
 t_color	diffuse_shading(t_data *data, double dot, int index)
 {
-	if (dot > 0)
-		return (col_multiply(data->objs[index].color, dot));
-	return ((t_color)(uint32_t)0);
+	return (col_multiply(data->objs[index].color, dot));
 }
 
 t_color	ambient_shading(t_data *data, int light_i, int index)
@@ -40,23 +38,44 @@ t_color	ambient_shading(t_data *data, int light_i, int index)
 	return (col_multiply(data->objs[index].color, data->lights[light_i].ambi));
 }
 
+t_color	specular_shading(t_data *data, t_inter inter, t_vec light)
+{
+	double	dot;
+	double	alpha;
+	t_vec	r;
+	t_vec	v;
+
+	alpha = 100;
+	r = vec_normalize(vec_substract(\
+			vec_multiply(inter.normal, dot_product(inter.normal, light) * 2.0),\
+			light));
+	v = vec_normalize(vec_substract(data->cams[data->i].pos, inter.ip));
+	dot = dot_product(v, r);
+	if (dot > 0)
+		return (col_multiply((t_color)(uint32_t)0xFFFFFF, pow(dot, alpha)));
+	return ((t_color)(uint32_t)0);
+}
+
 t_color	shade(t_data *data, t_inter inter, int light_i, int index)
 {
+	t_color	ret;
 	t_vec	light;
 	double	len;
 	double	dot;
-	int		nb;
 
+	ret = ambient_shading(data, light_i, index);
 	light = vec_substract(data->lights[light_i].pos, inter.ip);
 	len = get_length(light);
 	light = vec_normalize(light);
+	if (light_is_path_blocked(data, inter.ip, light, len))
+		return (ret);
 	inter.normal = data->objs[index].get_normal(data->objs[index], inter);
 	dot = dot_product(light, inter.normal);
-	if (dot >= 0 && is_light_path_blocked(data, inter.ip, light, len))
-		return (col_divide(ambient_shading(data, dot, index), 2));
-	return (blend_colors(\
-		diffuse_shading(data, dot, index), \
-		ambient_shading(data, light_i, index)));
+	if (dot <= 0)
+		return (ret);
+	ret = add_colors(ret, diffuse_shading(data, dot, index));
+	ret = add_colors(ret, specular_shading(data, inter, light));
+	return (ret);
 }
 
 int		get_px_color(t_data *data, t_inter inter, int index)
--- srcs/main.c
+++ srcs/main.c
@@ -6,50 +6,11 @@
 /*   By: mlantonn <mlantonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 17:49:38 by mlantonn          #+#    #+#             */
-/*   Updated: 2018/05/14 14:08:49 by mlantonn         ###   ########.fr       */
+/*   Updated: 2018/05/28 18:58:10 by mlantonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
-
-void	print_parsed_data(t_data data)
-{
-	for (int i = 0; i < 1; i++)
-	{
-		printf("CAM nb : %d;\n", i + 1);
-		printf("pos = %f, %f, %f\n", \
-			data.cams[i].pos.x, data.cams[i].pos.y, data.cams[i].pos.z);
-		printf("dir = %f, %f, %f\n", \
-			data.cams[i].dir.x, data.cams[i].dir.y, data.cams[i].dir.z);
-		printf("fov = %f\n\n", data.cams[i].fov);
-	}
-	for (int i = 0; i < data.nb_lights; i++)
-	{
-		printf("LIGHT nb : %d;\n", i + 1);
-		printf("is_para = %d\n", data.lights[i].is_para);
-		printf("color = %u, %u, %u, %u\n", data.lights[i].color.argb.r, \
-			data.lights[i].color.argb.g, data.lights[i].color.argb.b, data.lights[i].color.argb.a);
-		printf("r = %f\n", data.lights[i].r);
-		printf("pos = %f, %f, %f\n", \
-			data.lights[i].pos.x, data.lights[i].pos.y, data.lights[i].pos.z);
-		printf("dir = %f, %f, %f\n\n", \
-			data.lights[i].dir.x, data.lights[i].dir.y, data.lights[i].dir.z);
-	}
-	for (int i = 0; i < data.nb_objects; i++)
-	{
-		printf("OBJECT nb : %d;\n", i + 1);
-		printf("r = %f\n", data.objs[i].r);
-		printf("spec = %f\n", data.objs[i].spec);
-		printf("color = %u, %u, %u, %u\n", data.objs[i].color.argb.r, \
-			data.objs[i].color.argb.g, data.objs[i].color.argb.b, data.objs[i].color.argb.a);
-		printf("pos = %f, %f, %f\n", \
-			data.objs[i].pos.x, data.objs[i].pos.y, data.objs[i].pos.z);
-		printf("dir = %f, %f, %f\n", \
-			data.objs[i].dir.x, data.objs[i].dir.y, data.objs[i].dir.z);
-		printf("normal = %f, %f, %f\n\n", \
-			data.objs[i].normal.x, data.objs[i].normal.y, data.objs[i].normal.z);
-	}
-}
 
 int		main(int ac, char **av)
 {
@@ -63,7 +24,6 @@ int		main(int ac, char **av)
 	}
 	data_init(&data, ac, av);
 	draw_image(&data);
-	//print_parsed_data(data);
 	ft_event(&data);
 	return (0);
 }

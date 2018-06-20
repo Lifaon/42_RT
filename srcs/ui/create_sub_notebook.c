76323214c63207eb2af108e31d52bf71073841dc
Fabien CHEVREY <fchevrey@student.42.fr>
2018-06-20 18:55:38 +0200

    fixing a bug on value interchanging x and z on vector light.pos causing by the User Interface

--- srcs/ui/create_sub_notebook.c
+++ srcs/ui/create_sub_notebook.c
@@ -6,7 +6,7 @@
 /*   By: fchevrey <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/27 19:23:33 by fchevrey          #+#    #+#             */
-/*   Updated: 2018/06/18 20:35:04 by fchevrey         ###   ########.fr       */
+/*   Updated: 2018/06/20 18:52:08 by fchevrey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 int		create_sub_notebook(t_ui *ui)
 {
+	g_data->ui->is_active = 0;
 	if (!(create_light_ui(ui->tab)))
 		return (0);
 	if (!(create_camera_ui(ui->tab)))
 		return (0);
 	if (!(create_object_ui(ui->tab)))
 		return (0);
+	g_data->ui->is_active = 1;
 	return (1);
 }

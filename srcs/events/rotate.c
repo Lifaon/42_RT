32c13187c3432826910298fffcef3ab7e910ca0a
mlantonn <mlantonn@student.42.fr>
2018-06-20 22:02:05 +0200

    modified rotations/translations to work with json editor

--- srcs/events/rotate.c
+++ srcs/events/rotate.c
@@ -6,7 +6,7 @@
 /*   By: mlantonn <mlantonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/04 16:52:29 by mlantonn          #+#    #+#             */
-/*   Updated: 2018/06/20 00:14:01 by mlantonn         ###   ########.fr       */
+/*   Updated: 2018/06/20 20:16:58 by mlantonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	rotate_up(t_data *data)
 	data->cam.angle.x -= ANGLE;
 	if (data->cam.angle.x <= -360.0)
 		data->cam.angle.x += 360.0;
+	data->cams[data->i] = data->cam;
 }
 
 void	rotate_down(t_data *data)
@@ -25,6 +26,7 @@ void	rotate_down(t_data *data)
 	data->cam.angle.x += ANGLE;
 	if (data->cam.angle.x >= 360.0)
 		data->cam.angle.x -= 360.0;
+	data->cams[data->i] = data->cam;
 }
 
 void	rotate_left(t_data *data)
@@ -32,6 +34,7 @@ void	rotate_left(t_data *data)
 	data->cam.angle.y += ANGLE;
 	if (data->cam.angle.y >= 360.0)
 		data->cam.angle.y -= 360.0;
+	data->cams[data->i] = data->cam;
 }
 
 void	rotate_right(t_data *data)
@@ -39,4 +42,5 @@ void	rotate_right(t_data *data)
 	data->cam.angle.y -= ANGLE;
 	if (data->cam.angle.y <= -360.0)
 		data->cam.angle.y += 360.0;
+	data->cams[data->i] = data->cam;
 }

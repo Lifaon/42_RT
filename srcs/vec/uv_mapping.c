5e9c61e1b593255386f091b5c26b2d24d6ccc0ca
Meriadec LANTONNET <mlantonn@student.42.fr>
2018-06-29 20:37:21 +0200

    mistake pushing

--- srcs/vec/uv_mapping.c
+++ srcs/vec/uv_mapping.c
@@ -6,7 +6,7 @@
 /*   By: mlantonn <mlantonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/27 16:56:00 by mlantonn          #+#    #+#             */
-/*   Updated: 2018/06/29 04:45:52 by mlantonn         ###   ########.fr       */
+/*   Updated: 2018/06/29 20:36:52 by mlantonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,34 @@ static int	uv_mapping_plane(t_obj obj, t_inter *inter)
 	vp_len = get_length(vp);
 	vp = vec_normalize(vp);
 	u = (int)(dot_product(vp, obj.y_dir) * vp_len) + (obj.tex->size.x / 2);
-	v = (int)(dot_product(vp, obj.z_dir) * vp_len) + (obj.tex->size.y / 2);
+	v = (int)(dot_product(vp, obj.z_dir) * vp_len) \
+		+ (obj.tex->size.y / 2);
 	if (u >= 0 && u < obj.tex->size.x && v >= 0 && v < obj.tex->size.y)
 		return (obj.tex->pxl[u + (v * obj.tex->size.x)]);
+	return (obj.color.c);
+}
+
+static int	uv_mapping_cyl_cone(t_obj obj, t_inter *inter)
+{
+	t_vec	vp;
+	double	phi;
+	double	vp_len;
+	double	u;
+	int		v;
+
+	vp = vec_substract(inter->ip, obj.pos);
+	vp_len = get_length(vp);
+	vp = vec_normalize(vp);
+	phi = acos(dot_product(obj.dir, vp));
+	u = (acos(dot_product(vp, obj.y_dir) / sin(phi))) / (2 * M_PI);
+	if (dot_product(obj.z_dir, vp) < 0)
+		u = 1 - u;
+	v = -(int)(dot_product(obj.dir, vp) * vp_len) % obj.tex->size.y;
+	if (v < 0)
+		v = obj.tex->size.y + v;
+	if (u >= 0 && u <= 1 && v >= 0 && v < obj.tex->size.y)
+		return (obj.tex->pxl[(int)(obj.tex->size.x * u) \
+		+ (v * obj.tex->size.x)]);
 	return (obj.color.c);
 }
 
@@ -52,5 +77,7 @@ int		uv_mapping(t_obj obj, t_inter *inter)
 		return (uv_mapping_sphere(obj, inter));
 	else if (obj.obj_type == PLANE)
 		return (uv_mapping_plane(obj, inter));
+	else if (obj.obj_type == CYLINDER || obj.obj_type == CONE)
+		return (uv_mapping_cyl_cone(obj, inter));
 	return (obj.color.c);
 }

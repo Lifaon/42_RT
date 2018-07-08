7fedf48cb3dec159ab995dd834218a09ee33ddda
Dremelys <vtudes@student.42.fr>
2018-07-08 18:54:53 +0200

    hmmmmmmm

--- srcs/draw/perlin_distrib.c
+++ srcs/draw/perlin_distrib.c
@@ -1,15 +1,29 @@
-void		plan_perlin(t_inter inter, t_perlin p, t_obj obj)
+void		apply_perlin(t_inter inter, t_perlin p, t_obj obj)
 {
 	int a;
 
 	a = 2;
-	if (p.perlin == 3)
-	{
-		inter.normal.z = (inter.normal.z + noise_3D(inter.ip.z - a, inter.ip.y, inter.ip.x, p))
-		- (inter.normal.z + noise_3D(inter.ip.z + a, inter.ip.y, inter.ip.z, p));
-		 inter.normal.y = (inter.normal.y + noise_3D(inter.ip.z, inter.ip.y - a, inter.ip.x, p))
-		- (inter.normal.y + noise_3D(inter.ip.z, inter.ip.y + a, inter.ip.x));
-		inter.normal.x = (inter.nornal.x + noise3D(inter.ip.z, inter.ip.y, inter.ip.x - a)) 
-		- (inter.normal.x + noise_3D(inter.ip.z, inter.ip.y, inter.ip.x + a));
-	}
+
+	inter.normal.z = (inter.normal.z + noise_3D(inter.ip.z - a, inter.ip.y, inter.ip.x, p))
+	- (inter.normal.z + noise_3D(inter.ip.z + a, inter.ip.y, inter.ip.z, p));
+	 inter.normal.y = (inter.normal.y + noise_3D(inter.ip.z, inter.ip.y - a, inter.ip.x, p))
+	- (inter.normal.y + noise_3D(inter.ip.z, inter.ip.y + a, inter.ip.x));
+	inter.normal.x = (inter.nornal.x + noise_3D(inter.ip.z, inter.ip.y, inter.ip.x - a)) 
+	- (inter.normal.x + noise_3D(inter.ip.z, inter.ip.y, inter.ip.x + a));
+}
+
+void		call_perlin(t_data data)
+{
+	t_obj obj;
+	t_perlin p;
+	t_inter inter;
+
+	if (obj.obj_type == PLAN)
+		apply_perlin(inter, p, obj);
+	if (obj.obj_type == SPHERE)
+		apply_perlin(inter, p, obj);
+	if (obj.obj_type == CONE)
+		apply_perlin(inter, p, obj);
+	if (obj.obj_type == CYLINDER)
+		apply_perlin(inter, p, obj);
 }

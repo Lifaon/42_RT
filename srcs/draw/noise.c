e68a226d503e70630af2fb6cde72d364f8b98946
Dremelys <vtudes@student.42.fr>
2018-07-19 05:52:08 +0200

    Version finale Perlin, avec cos

--- srcs/draw/noise.c
+++ srcs/draw/noise.c
@@ -13,11 +13,12 @@ void	ft_perlin(t_data *data, t_color *color, t_inter *inter)
 	vec[1] = inter->ip.y * 0.05;
 	vec[2] = inter->ip.z * 0.05;
 	level = 1;
-	while (++level < 15)
+	while (++level < 40)
 	{
 		coef += (1.0 / level) * fabs(noise_vec(vec));
 	}
-	coef = 0.42 * coef + 0.8;
+	coef = 0.2 * cos((inter->ip.x + inter->ip.y + inter->ip.z) * 0.05 + coef) + 0.9;
+	//coef = 0.42 * coef + 0.8;
 	if (coef < 0)
 		coef = 0;
 	else if (coef > 1)
